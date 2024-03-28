2dc8ba008777891a915ac312378bba8ef10ff32f
LuisKolb <kolb.luis@gmail.com>
2024-03-28 22:03:55 +0100

    refactor and split up util functions

---
+++ clef/utils/data_loading.py
@@ -0,0 +1,138 @@
+from typing import List, Tuple, Dict, TypedDict, Union, Optional, NamedTuple
+import json
+import os
+
+class RumorDict(TypedDict):
+    id: str
+    rumor: str
+    label: str
+    timeline: List[List[str]]
+    evidence: List[List[str]]
+    retrieved_evidence: List[List[Union[str, int, float]]]
+
+class RankedDocs(NamedTuple):
+    author_account: str
+    authority_tweet_id:str
+    doc_text: str
+    rank: int
+    score: float
+
+
+def load_rumors_from_jsonl(filepath: Union[str, os.PathLike]) -> List[RumorDict]:
+    jsons = []
+    with open(filepath, encoding='utf8') as file:
+        for line in file:
+            jsons += [json.loads(line)]
+    return jsons
+
+
+def write_trec_format_output(filename: str, data: List[List[Union[str, int, float]]], tag: str) -> None:
+    """
+    Writes data to a file in the TREC format.
+
+    Parameters:
+    - filename (str): The name of the file to write to.
+    - data (List[Tuple[str, int, int, float]]): A list of tuples, where each tuple contains:
+        - rumor_id (str): The unique ID for the given rumor.
+        - authority_tweet_id (int): The unique ID for the authority tweet.
+        - rank (int): The rank of the authority tweet ID for that given rumor_id.
+        - score (float): The score given by the model for the authority tweet ID.
+    - tag (str): The string identifier of the team/model.
+    """
+    with open(filename, 'w') as file:
+        for rumor_id, authority_tweet_id, rank, score in data:
+            line = f"{rumor_id}\tQ0\t{authority_tweet_id}\t{rank}\t{score}\t{tag}\n"
+            file.write(line)
+
+
+def combine_rumors_with_trec_file_judgements(jsons, trec_judgements_path):
+    """
+    create a list of RankedDocs objects in key retrieved_evidence from TREC-formatted file
+
+    Parameters:
+        - jsons: list of json-like rumors from dataset
+        - trec_judgements_path: filepath to TREC-formatted file containing rank and score
+
+    Returns:
+    list of json-like rumors from dataset, with the key retrieved_evidence populated with a list of RankedDocs-like objects
+    """
+    trec_by_id = {}
+
+    with open(trec_judgements_path, 'r') as file:
+        for line in file:
+            rumor_id, _, evidence_id, rank, score, _ = line.split('\t')
+            if rumor_id not in trec_by_id:
+                trec_by_id[rumor_id] = {}
+            trec_by_id[rumor_id][evidence_id] = (rank, score)
+
+    for i, item in enumerate(jsons):
+        item['retrieved_evidence'] = []
+
+        doc_ranks = trec_by_id[item['id']]
+        for author_account, tweet_id, tweet_text in item['timeline']:
+            if tweet_id in doc_ranks:
+                rank, score = doc_ranks[tweet_id]
+                item['retrieved_evidence'] += [[
+                    author_account,
+                    tweet_id,
+                    tweet_text,
+                    int(rank),
+                    float(score),
+                ]]
+        
+        jsons[i] = item
+    return jsons
+
+
+def write_jsonlines_from_dicts(filename: Union[str, os.PathLike], dicts: List[Dict]) -> None:
+    with open(filename, 'w') as file:
+        for item in dicts:
+            file.write(f'{json.dumps(item)}\n')
+
+
+def clean_jsons(jsons):
+    from .preprocessing import clean_tweet
+
+    data_cleaned = []
+
+    for entry in jsons:
+        
+        tl_clean = []
+        for account_url, tl_tweet_id, tl_tweet in entry['timeline']:
+            tl_tweet_cleaned = clean_tweet(tl_tweet)
+            if tl_tweet_cleaned:
+                tl_clean += [[account_url, tl_tweet_id, tl_tweet_cleaned]]
+
+        ev_clean = []
+        for account_url, ev_tweet_id, ev_tweet in entry['evidence']:
+            ev_tweet_cleaned = clean_tweet(ev_tweet)
+            if ev_tweet_cleaned:
+                ev_clean += [[account_url, ev_tweet_id, ev_tweet_cleaned]]
+
+        data_cleaned += [{
+            'id': entry['id'],
+            'rumor': clean_tweet(entry['rumor']),
+            'label': entry['label'],
+            'timeline': tl_clean,
+            'evidence': ev_clean,
+        }]
+    return data_cleaned
+
+clef_base_path = '../clef2024-checkthat-lab/task5'
+
+def load_datasets(preprocess: bool, clef_path: str = clef_base_path):
+    
+    data_path = os.path.join(clef_path, 'data')
+
+    filepath_train = os.path.join(data_path, 'English_train.json')
+    filepath_dev = os.path.join(data_path, 'English_dev.json')
+
+    train_jsons = load_rumors_from_jsonl(filepath_train)
+    dev_jsons = load_rumors_from_jsonl(filepath_dev)
+
+    print(f'loaded {len(train_jsons)} training json lines and {len(dev_jsons)} dev json lines.')
+
+    if preprocess:
+        return (clean_jsons(train_jsons), clean_jsons(dev_jsons))
+    else:
+        return (train_jsons, dev_jsons)
