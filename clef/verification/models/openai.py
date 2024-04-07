039834599be0b7cf8868096fc0fcad800ade4b15
LuisKolb <kolb.luis@gmail.com>
2024-04-07 16:24:23 +0200

    update prompt and rerun

--- clef/verification/models/openai.py
+++ clef/verification/models/openai.py
@@ -14,8 +14,9 @@ client = OpenAI(
 )
 
 system_message = """
-You are a helpful assistant. 
+You are a helpful assistant.
 You need to decide if a statement by a given source either supports the given claim ("SUPPORTS"), refutes the claim ("REFUTES"), or if the premise is not related to the claim ("NOT ENOUGH INFO"). 
+USE ONLY THE PROVIDED STATEMENT TO MAKE YOUR DECISION.
 You must also provide a confidence score between 0 and 1, indicating how confident you are in your decision.
 Format your answer in JSON format, like this: {"decision": ["SUPPORTS"|"REFUTES"|"NOT ENOUGH INFO"], "confidence": [0...1]}
 Set your own temperature to 0.
